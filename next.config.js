e73e802b293508eb896a1c754baa44de42220e4a
Mateus Leite Fausto <fausto.dataanalytics@gmail.com>
2026-04-19 21:19:55 -0300

    fix: add basePath and assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Se for hospedar em username.github.io/repo-name, ajuste o basePath:
-  // basePath: '/nome-do-repositorio',
+  basePath: '/uxecognicao',
+  assetPrefix: '/uxecognicao/',
   images: {
     unoptimized: true,
   },
